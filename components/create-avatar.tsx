15a15286f9c8a85def9273ed3cae0db8b26a8b43
LinkangZhan <linkangzhan@foxmail.com>
2023-10-16 02:30:34 -0400

    change post url

--- components/create-avatar.tsx
+++ components/create-avatar.tsx
@@ -80,7 +80,7 @@ export function CreateAvatar() {
         };
 
         try {
-            const response = await fetch('http://localhost:5328/api/submit', {
+            const response = await fetch('/api/submit', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(formData)  // 提交所有的状态数据
